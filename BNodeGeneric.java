bcd628cf128381372df5b18bf08d5453d7a973f2
Anthony Joaquin Ticona Hareth <aticonaha@unsa.edu.pe>
2022-08-06 15:40:14 -0500

    Método add e insertNotFull

--- BNodeGeneric.java
+++ BNodeGeneric.java
@@ -16,5 +16,31 @@ public class BNodeGeneric<E> {
         this.num = 0;
     }
 
+    public void insertNotFull(E key){
+
+        int i = num -1; // Initialize i as the rightmost index
+
+        if (isLeaf){ // When it is a leaf node
+            // Find the location where the new key should be inserted
+            while (i >= 0 && keys.get(i).compareTo(key) > 0){
+                keys.set(i+1, keys.get(i)); // keys backward shift
+                i--;
+            }
+            keys.set(i+1, key);
+            num++;
+        }
+        else{
+            // Find the child node location that should be inserted
+            while (i >= 0 && keys.get(i).compareTo(key) > 0)
+                i--;
+            if (children.get(i+1).num == 2*MinDeg - 1){ // When the child node is full
+                splitChild(i+1,children.get(i+1));
+                // After splitting, the key in the middle of the child node moves up, and the child node splits into two
+                if (keys.get(i+1).compareTo(key) < 0)
+                    i++;
+            }
+            children.get(i+1).insertNotFull(key);
+        }
+    }
     
 }
--- BTree.java
+++ BTree.java
@@ -1,10 +1,39 @@
 public class BTree<E extends Comparable<E>> {
 
     BNodeGeneric<E> root;
+    int MinDeg;
+
+    // Constructor
+    public BTree(int deg){
+        this.root = null;
+        this.MinDeg = deg;
+    }
 
     public boolean add(E value) {
-        //TODO implement here!
-        return false;
+        if (root == null){
+            root = new BNodeGeneric<E>(MinDeg,true);
+            root.keys.set(0, value);
+            root.num = 1;
+        }
+        else {
+            // When the root node is full, the tree will grow high
+            if (root.num == 2*MinDeg-1){
+                BNodeGeneric<E> s = new BNodeGeneric<E>(MinDeg,false);
+                // The old root node becomes a child of the new root node
+                s.children.set(0, root);
+                // Separate the old root node and give a key to the new node
+                s.splitChild(0,root);
+                // The new root node has 2 child nodes. Move the old one over there
+                int i = 0;
+                if (s.keys.get(0).compareTo(value) < 0)
+                    i++;
+                s.children.get(i).insertNotFull(value);
+
+                root = s;
+            }
+            else
+                root.insertNotFull(value);
+        }
     }
 
     public E remove(E value) {
